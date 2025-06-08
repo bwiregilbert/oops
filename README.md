4070d944084cf4bdcbfd5ecd6065cebe27fc3e68
Bwire Gilbert <williambwire84@gmail.com>
2025-06-08 19:19:59 +0300

    Initial oop commit short progrma

---
+++ README.md
@@ -0,0 +1,5 @@
+# My first object oriented programming code
+This program it define the class named student together with the attributes like name ,
+age,course . well with the method called display_info to print student's details in this 
+format: Name: [name], Age: [age], Course: [course].
+Also created two objects named Alice and Ben .Finally call the display_info method for both objects.
---
+++ py_ops.py
@@ -0,0 +1,81 @@
+import os 
+os.system('cls')
+                        
+class student:
+    def __init__ (self,name,age,course):
+        self.name = name
+        self.age = age
+        self.course = course
+    def display_info(self):
+        print(f'Name: {[self.name]}, Age: {[self.age]}, Course: {[self.course]}')
+Alice = student('Alice',26,'Information Technology')
+Ben = student('Ben',24,'Cyber security') 
+Alice.display_info()
+Ben.display_info()                   
+        
+        
+                 
+    
+        
+         
+
+    
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
